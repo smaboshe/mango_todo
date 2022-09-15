db9a0e0aba3332e078921aec7f68df8ba3d20f0d
Silumesii Maboshe <silumesii@thoughtbot.com>
2022-09-15 21:49:03 +0200

    User creates a todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :todos, only: [:new, :create, :index]
+
   root "todos#index"
 end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+    click_on "Add a new todo"
+
+    # page.send_keys("Buy milk", :enter)
+    fill_in "Title", with: "Buy milk"
+    click_on "Create Todo"
+
+    expect(page).to have_css ".todos li", text: "Buy milk"
+  end
+end
